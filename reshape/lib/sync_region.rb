35ba0b9ddf4eea8d9e0154b29a2c9c0d73acb6a4
Hooopo Wang <hoooopo@gmail.com>
2023-02-15 06:47:51 +0800

    Fix region sync & display

--- reshape/lib/sync_region.rb
+++ reshape/lib/sync_region.rb
@@ -1,8 +1,14 @@
 class SyncRegion 
   def run 
-    Star.where("location is not null and location <> ''").each do |star|
+    Star.where("location is not null and location <> '' and region is null").limit(5000).each do |star|
       region = Geocoder.search(star.location)&.first&.country
-      Star.where(repo_id: star.repo_id, user_id: star.user_id).update_all(region: region) if region.present?
+      if region.present?
+        puts star.location
+        Star.where(repo_id: star.repo_id, user_id: star.user_id).update_all(region: region) 
+      else
+        puts star.location
+        Star.where(repo_id: star.repo_id, user_id: star.user_id).update_all(region: 'N/A') 
+      end
     end
   end
 end

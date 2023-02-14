4fe77521935069613efc155c13a901543b9c9203
Hooopo Wang <hoooopo@gmail.com>
2023-02-15 06:58:11 +0800

    rm debug info

--- reshape/lib/sync_region.rb
+++ reshape/lib/sync_region.rb
@@ -3,10 +3,8 @@ class SyncRegion
     Star.where("location is not null and location <> '' and region is null").limit(5000).each do |star|
       region = Geocoder.search(star.location)&.first&.country
       if region.present?
-        puts star.location
         Star.where(repo_id: star.repo_id, user_id: star.user_id).update_all(region: region) 
       else
-        puts star.location
         Star.where(repo_id: star.repo_id, user_id: star.user_id).update_all(region: 'N/A') 
       end
     end

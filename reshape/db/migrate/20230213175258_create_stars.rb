a48bf8c5a5ef7906b257f1719cd180b16af27120
Hooopo Wang <hoooopo@gmail.com>
2023-04-13 00:40:32 +0800

    add users

--- reshape/db/migrate/20230213175258_create_stars.rb
+++ reshape/db/migrate/20230213175258_create_stars.rb
@@ -1,18 +1,10 @@
 class CreateStars < ActiveRecord::Migration[7.0]
   def change
-    create_table :stars, id: false do |t|
-      t.bigint :repo_id 
-      t.bigint :user_id
-      # avatar_url -> https://avatars.githubusercontent.com/u/19?size=50
-      
-      t.string :login 
-      t.string :location 
-      t.string :company
-      
-      t.datetime :starred_at
-      t.timestamps
-
-      t.index [:repo_id, :user_id], unique: true
+    create_table :starred_repos, id: false do |t|
+      t.bigint "user_id", null: false
+      t.bigint "repo_id", null: false
+      t.datetime "starred_at", null: false
+      t.index ["user_id", "repo_id"], name: "index_starred_repos_on_user_id_and_repo_id", unique: true
     end
   end
 end
